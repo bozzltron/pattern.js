51e0bb16893ff779dba0d3d8877ed5d29509f2e9
Michael Bosworth <michael.bosworth@bunchball.com>
2017-01-07 12:14:16 -0600

    initial code

---
+++ pattern.js
@@ -0,0 +1,61 @@
+'use strict';
+
+class Pattern {
+	
+	constructor(){
+		this.pattern = [];
+	}
+
+	detect(first, second) {
+
+		let index = 0, 
+			secondIndex = 0,
+			fragmentLength = 2,
+			patterns = [];
+
+		// make sure the first is the shorter string
+		if(first.length > second.length) {
+			let temp = first;
+			first = second;
+			second = temp;
+		}
+
+		while(index <= first.length - 1 ) {
+
+			// get 2 chars relative to the index
+			let lookahead = first.substring(index, index + fragmentLength),
+				patternIndex = second.substring(secondIndex).indexOf(lookahead),
+				pattern = '',
+				length = fragmentLength;
+
+			// keep adding chars to the comparator till we reach the end of the pattern
+			while(patternIndex != -1 && ((patternIndex + length - 1) <=	 second.length -1 )) {
+				length++;
+				pattern = lookahead;
+				lookahead = first.substring(index, index + length);
+				patternIndex = second.substring(secondIndex).indexOf(lookahead);
+			}
+
+			// if we found a pattern
+			if(pattern.length > 0) {
+
+				// save it
+				patterns.push(pattern);
+
+				// move the index ahead accordingly
+				index += patternIndex + pattern.length;
+
+				secondIndex = patternIndex;
+
+			} else {
+				index++;
+			}
+
+		}
+
+		return patterns;
+	}
+
+}
+
+module.exports = Pattern;
---
+++ spec/pattern.spec.js
@@ -0,0 +1,27 @@
+
+var Pattern = require('../pattern'),
+	pattern = new Pattern();
+
+describe('Pattern', function() {
+
+	beforeEach(function() {
+		pattern = new Pattern();
+	});
+
+	it('should detect patterns', function() {
+		expect(pattern.detect('abc', 'xbc')).toEqual(['bc']);
+		pattern = new Pattern();
+		expect(pattern.detect('abcde', 'xbcdef')).toEqual(['bcde']);
+	});
+
+	it('should give the same result for the same strings passed in different order', function(){
+		expect(pattern.detect('<a href="http://www.google.com">Go to Google</a>', 'http://www.google.com')).toEqual(['http://www.google.com']);
+		pattern = new Pattern();
+		expect(pattern.detect('http://www.google.com', '<a href="http://www.google.com">Go to Google</a>')).toEqual(['http://www.google.com']);
+	});
+
+	it('should detect mulitple patterns', function(){
+		expect(pattern.detect('<a href="http://www.google.com">Google</a><a href="http://www.twitter.com">Twitter</a>', 'google twitter')).toEqual(['google', 'twitter']);
+	});
+
+});
